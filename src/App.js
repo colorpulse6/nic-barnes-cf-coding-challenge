78b589e7d2f75700d276f3980ec39b4a0aca1633
Nic Barnes <colorpulse@gmail.com>
2020-08-18 09:42:56 +0200

    fetch courses

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React from "react";
 
+class App extends React.Component {
+  state = {
+    courses: [],
+  };
+  
+  componentDidMount() {
+    this.fetchCourses();
+  }
 
-function App() {
-  return (
-    <div className="App">
-      CF Challenge
-    </div>
-  );
+  fetchCourses() {
+    fetch("https://private-e05942-courses22.apiary-mock.com/courses")
+      .then((res) => res.json())
+      .then(
+        (courses) => {
+          this.setState({ courses: courses });
+        },
+        (error) => console.log(error)
+      );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.courses.map((course, index) => {
+          return <div key={index}>{course.title}</div>;
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
